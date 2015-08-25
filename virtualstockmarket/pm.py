782c4883c9a6d3082a03e7b50ce6018e8a1189f3
b0wmz1337 <b0wmz1337@users.noreply.github.com>
2015-08-24 22:58:32 -0700

    show credit balance in pm

--- virtualstockmarket/pm.py
+++ virtualstockmarket/pm.py
@@ -5,15 +5,23 @@ import share
 s = share.STOCKS("")
 
 s.getTotalShares()
+s.getUsersCredit()
 messages = s.r.get_unread()
 for m in messages:
 	if m.body.lower() == "list":
-		message = ["%s, your current shares are as follows:\n\n" % str(m.author)]
+		message = []
 		shares = s.getUserShares(str(m.author))
 		if shares is None:
-			message = "%s, you have no shares." % str(m.author)
+			message.append("%s, you have no shares." % str(m.author))
 		else:
+			message.append("%s, your current shares are as follows:\n\n" % str(m.author))
 			for idx, val in shares.iteritems():
 				message.append("%s: %s  \n" % (idx, val))
+		
+		credit = s.getUserCredit(str(m.author))
+		if credit is None:
+			message.append("\n\nYou do not appear to have a credit account. Please comment at least once to get one set up")	
+		else:
+			message.append("\n\nYour credit balance is: %d" % credit)
 		s.log.debug(''.join(message))
 		m.reply(''.join(message))
